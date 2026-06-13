b10b3421c2b4ad9c38bf795ce594efb8c40dbd3e
wizkid-opsec <mishka280791@gmail.com>
2026-06-13 19:31:37 +0200

    Tag 1: Erste Python Grundlagen

---
+++ tag01_grundlagen.py
@@ -0,0 +1,18 @@
+# Tag 1 - Python Grundlagen
+
+# Variablen
+name = "wizkid"
+alter = 34
+ist_hacker = True
+
+# Ausgabe
+print("Hallo, ich bin", name)
+print("Ich bin", alter, "Jahre alt")
+print("Hacker-Modus:", ist_hacker)
+
+# Einfache Rechnung
+a = 10
+b = 3
+print("Addition:", a + b)
+print("Division:", a / b)
+print("Rest:", a % b)  # Modulo - wichtig für Security!
